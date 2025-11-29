73f5368c61491bc2d48ffab972bf964e32baf634
Gopal krishn sahu <145195848+gopalsahu1699@users.noreply.github.com>
2025-11-29 11:05:19 +0530

    logo update

--- assets/assets.js
+++ assets/assets.js
@@ -1,5 +1,6 @@
 export const assets = {
-  logo: "https://res.cloudinary.com/dn9rohd6h/image/upload/v1764388210/Logo_for_Automensor_140_40_webp_z5i2fn.webp",
+  // logo: "https://res.cloudinary.com/dn9rohd6h/image/upload/v1764388210/Logo_for_Automensor_140_40_webp_z5i2fn.webp",
+  logo: "https://res.cloudinary.com/dn9rohd6h/image/upload/v1764392811/Logo2_for_Automensor_iljsho.webp",
   home_banner: "https://res.cloudinary.com/dn9rohd6h/image/upload/v1762664809/home_kjbuox.webp",
   estimate_cost_home: "https://res.cloudinary.com/dn9rohd6h/image/upload/estimate_cost_home_u364ow.webp",
   estimate_cost_category_1: "https://res.cloudinary.com/dn9rohd6h/image/upload/estimate_cost_category_1_elre4r.webp",
@@ -10,5 +11,5 @@ export const assets = {
   slide_img_3: "https://res.cloudinary.com/dn9rohd6h/image/upload/slide_img_3_rlzsw9.webp",
 
   // Add other icon URLs here, or continue using local imports for SVGs/icons
-  // logout, search_icon, etc.
+
 };
